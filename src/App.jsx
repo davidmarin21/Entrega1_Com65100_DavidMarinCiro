253ae3e4b11685920625ebff3cc145648970bcd8
David Marin Ciro <daviidciro@gmail.com>
2024-09-16 16:59:50 -0500

    segunda entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import NavBar from './componentes/Navbar';
-import ItemListContainer from './componentes/ItemListContainer';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer';
 
 const App = () => {
   return (
     <div>
       <NavBar />
-      <ItemListContainer mensaje="Encuentra todo en tecnología" />
+      <Routes>
+        <Route path="/" element={<ItemListContainer greeting="Elija sus productos" />} />
+        <Route path="/category/:categoryId" element={<ItemListContainer />} />
+        <Route path="/item/:itemId" element={<ItemDetailContainer />} />
+        <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+      </Routes>
     </div>
   );
 };
